ea683951b697423facfe70a6e2b51636694cedfd
Ninja_coder <ankursinha1512@gmail.com>
2026-08-29 00:57:52 +0530

    Add: valid-word-abbreviation - submission-0

---
+++ Data Structures & Algorithms/valid-word-abbreviation/submission-0.java
@@ -0,0 +1,39 @@
+class Solution {
+    public boolean validWordAbbreviation(String word, String abbr) {
+        int i = 0;
+        int j = 0;
+
+        while (i < word.length() && j < abbr.length()) {
+
+            // If abbreviation starts with a digit
+            if (Character.isDigit(abbr.charAt(j))) {
+
+                // Leading zero is invalid
+                if (abbr.charAt(j) == '0') {
+                    return false;
+                }
+
+                int num = 0;
+
+                // Build the complete number
+                while (j < abbr.length() && Character.isDigit(abbr.charAt(j))) {
+                    num = num * 10 + (abbr.charAt(j) - '0');
+                    j++;
+                }
+
+                i += num;
+            } 
+            else {
+                // Letter must match
+                if (word.charAt(i) != abbr.charAt(j)) {
+                    return false;
+                }
+
+                i++;
+                j++;
+            }
+        }
+
+        return i == word.length() && j == abbr.length();
+    }
+}
